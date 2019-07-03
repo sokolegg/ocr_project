cad33e3aa25ebf05bb3506a38d35fdebb2c2c580
sokolegg <sokolegg@yandex.ru>
2019-07-03 14:29:27 +0300

    rtf info fixed, new usage with path converting (README changed), strings now decoding from b-strings normally

--- README.md
+++ README.md
@@ -20,8 +20,10 @@ cd textract
 pip install .
 
 # Usage
+files:
 python converter.py ./test_files/test.docx test_docx.pdf
 python converter.py ./test_files/test.xlsx test_xlsx.pdf
-python converter.py ./test_files/test.doc test_doc.pdf
-python converter.py ./test_files/test.xls test_xls.pdf
-python converter.py ./test_files/test.rtf test_rtf.pdf
+
+convert folder with files:
+python converter.py ./test_files pdf
+-- it's create test_files_pdf with all files from test_files converted to pdf format
--- converter.py
+++ converter.py
@@ -5,8 +5,9 @@
 import sys
 import textract
 from fpdf import FPDF
+import os
 
-def str2pdf(str_obj, filename='string.pdf'):   
+def str2pdf(str_obj, filename='last_text.pdf'):   
 	pdf = FPDF()
 	pdf.add_page()
 	pdf.set_font("Arial", size=12)
@@ -14,15 +15,48 @@ def str2pdf(str_obj, filename='string.pdf'):
 	pdf.output(filename)
 	return open(filename, "rb").read()
 
+def str2txt(str_obj, filename='last_text.txt'):
+	txt = open(filename, 'w')
+	txt.write(str_obj)
+	txt.close()
+	return str_obj
+
 def file2str(filename):
 	text = textract.process(filename)
-	return str(text)
+	return remove_rtf_info(text.decode('utf-8'))
+
+def remove_rtf_info(str_obj):
+	return str_obj.split('-----------------')[-1]
+
+
+def convert_file(file_in, file_out):
+	text = file2str(file_in)
+	print('Text in file ', file_in, ' : ', text[:50], '...')
+
+	# write to specific format
+	if file_out.endswith('pdf'):
+		returned_info = str2pdf(text, file_out)
+	else:
+		returned_info = str2txt(text, file_out)
+	print('File data: ', returned_info[:50], '... saved as: ', file_out)
+
+def convert_path(path, convert_format):
+	convert_path = path + '_' + convert_format
+	if not os.path.isdir(convert_path):
+		os.mkdir(convert_path)
+
+	for file_in in os.listdir(path):
+		full_file_in = f"{path}/{file_in}"
+		full_file_out = f"{convert_path}/{file_in}.{convert_format}"
+		convert_file(full_file_in, full_file_out)
+
 
 if __name__ == '__main__':
 	args = sys.argv[1:]
-	file_in = args[0]
-	file_out = args[1]
-	text = file2str(file_in)
-	print('Text in file ', file_in, ' : ', text[:50], '...')
-	pdf = str2pdf(text, file_out)
-	print('Pdf : ', pdf[:50], '... saved as: ', file_out)
+	if os.path.isdir(args[0]):
+		convert_path(args[0], args[1])
+	else:
+		convert_file(args[0], args[1])
+
+
+
