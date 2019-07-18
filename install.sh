0a6c004675f57b4ed7d62f4b7a9a4990acb18f27
sokolegg <sokolegg@yandex.ru>
2019-07-18 23:40:38 +0300

    install.sh fixed

---
+++ install.sh
@@ -0,0 +1,3 @@
+apt-get install poppler-utils
+apt-get install tesseract-ocr libtesseract-dev libleptonica-dev pkg-config
+pip install .
--- ocr_pipe/install.sh
+++
@@ -1,2 +0,0 @@
-apt-get install poppler-utils
-apt-get install tesseract-ocr libtesseract-dev libleptonica-dev pkg-config
